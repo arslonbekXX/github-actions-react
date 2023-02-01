c2aca2337ca8acade5bbe116426ab1962585c083
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-02 01:39:30 +0500

    feat: workflow deployment

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -4,6 +4,7 @@ import Main from "./main";
 
 test("renders learn react link", () => {
 	render(<Main />);
+
 	const linkElement = screen.getByText(/learn react/i);
 	expect(linkElement).toBeInTheDocument();
 });
