ba94ee54aaf20776e738d237ffd445ba23454bfe
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-02 01:08:14 +0500

    add testing workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-interface AppProps {}
-
-const App: React.FC<AppProps> = () => {
-	const [count, setCount] = React.useState(0);
+function App() {
 	return (
-		<div>
-			<button onClick={() => setCount((v) => v - 1)}>Decrement</button>
-			<h1 style={{ display: "inline-block" }}>Count: {count}</h1>
-			<button onClick={() => setCount((v) => v + 1)}>Increment</button>
+		<div className='App'>
+			<header className='App-header'>
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+					Learn React
+				</a>
+			</header>
 		</div>
 	);
-};
+}
 
 export default App;
