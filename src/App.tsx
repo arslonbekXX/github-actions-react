a51c7ceb2cf4f314745d1af8de81380d0428f824
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-02 00:28:46 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+	const [count, setCount] = React.useState(0);
+	return (
+		<div>
+			<button onClick={() => setCount((v) => v - 1)}>Decrement</button>
+			<h1 style={{ display: "inline-block" }}>Count: {count}</h1>
+			<button onClick={() => setCount((v) => v + 1)}>Increment</button>
+		</div>
+	);
+};
 
 export default App;
