c9a194789a75581458db76d40ac1754789aa22bc
arslonbekXX <arslonbek.lion067@gmail.com>
2023-02-04 19:06:33 +0500

    feat: add increment btn

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,15 @@ interface MainProps {
 	cy: string;
 }
 
-const Main: React.FC<MainProps> = ({ name, cy }) => <h1 data-cy={cy}>Hello {name}</h1>;
+const Main: React.FC<MainProps> = ({ name, cy }) => {
+	const [count, setCount] = React.useState(0);
+
+	return (
+		<div>
+			<h1 data-cy={cy}>Hello {name}</h1>
+			<button onClick={() => setCount((v) => v + 1)}>count: {count}</button>
+		</div>
+	);
+};
 
 export default Main;
